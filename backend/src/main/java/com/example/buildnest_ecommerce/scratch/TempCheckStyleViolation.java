6549d68c1c53d901e45140797dd7850a711e02cb
pradip9096 <ghadgep196@gmail.com>
2026-07-15 19:54:59 +0530

    test: deliberately trip CheckStyle line-length limit to verify branch protection blocks merge (#408)

---
+++ backend/src/main/java/com/example/buildnest_ecommerce/scratch/TempCheckStyleViolation.java
@@ -0,0 +1,6 @@
+package com.example.buildnest_ecommerce.scratch;
+
+public class TempCheckStyleViolation {
+    public void thisLineIsDeliberatelyMadeVeryLongOnPurposeToTripTheCheckStyleLineLengthLimitForVerificationPurposesOnlyXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX() {
+    }
+}
